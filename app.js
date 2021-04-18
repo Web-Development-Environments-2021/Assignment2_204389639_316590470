98c03d9fcdd7a96488f7f9bfcc58b881f5e60c4d
shiritab <69760048+shiritab@users.noreply.github.com>
2021-04-18 14:01:38 +0300

    fixed menu

--- app.js
+++ app.js
@@ -194,4 +194,5 @@ function openPage(pageName, elmnt, color) {
   }
   
   // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+//   document.getElementById("defaultOpen").click();
+
